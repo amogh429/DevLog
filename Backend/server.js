d503e66a6279f3f95ffc1947a77a0232f6cab3b8
amogh429 <amogh.allrounder@gmail.com>
2026-07-04 20:32:05 +0530

    Initial Commit

---
+++ Backend/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+
+
+
+connectDB();
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
